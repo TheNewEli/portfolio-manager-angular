3df8ea820a43f14e98dcc12c4804aac4cd83d0e2
d_quaner <849358686>
2020-09-17 17:59:27 +0800

    modify - url

--- src/app/investments/investments.component.ts
+++ src/app/investments/investments.component.ts
@@ -7,9 +7,9 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./investments.component.css']
 })
 export class InvestmentsComponent implements OnInit {
-  cashAccount = new Array();
+  cashAccounts = new Array();
   investments = new Array();
-  netWorth: number;
+  netWorth = 0;
   totalCash: number;
   totalInvestment: number;
   today: number = Date.now();
@@ -21,9 +21,6 @@ export class InvestmentsComponent implements OnInit {
     // 后台数据的请求地址，如果变量定义后不再重新赋值，则应该使用const来定义
     this.getCash();
     this.getInvestment();
-    this.getTotalInvestment();
-    this.getTotalCash();
-    this.getNetWorth();
   }
 
   getCash(): void {
@@ -32,44 +29,32 @@ export class InvestmentsComponent implements OnInit {
     this.httpClient.get(url)
       .subscribe((response: any) => {
         console.log(response);
-        this.cashAccount = response;
+        this.cashAccounts = response;
+        this.totalCash = 0;
+        this.cashAccounts.forEach(val => {this.totalCash += val.value; });
+        this.netWorth += this.totalCash;
       }, (response) => {
         console.log(response);
         console.error('请求出错');
       });
-    // this.totalCash = 0;
-    // for (const cash of this.cashAccount) {
-    //   alert(1);
-    // }
-    // this.cashAccount.forEach((val, index) => {this.totalCash += val.value; alert(index); });
   }
 
   getInvestment(): void {
-    const url = 'http://localhost:8080/investmentinfo';
+    const url = 'http://localhost:8080/investmentinfo/test';
     // 使用get方法请求url，请求一旦成功后，将调用传入的第一个方法；如果请求失败，将调用传入的第二个方法
     this.httpClient.get(url)
       .subscribe((response: any) => {
         console.log(response);
         this.investments = response;
+        this.totalInvestment = 0;
+        this.investments.forEach(val => {this.totalInvestment += val.currentValue; });
+        this.netWorth += this.totalInvestment;
       }, (response) => {
         console.log(response);
         console.error('请求出错');
       });
-    // this.totalInvestment = 0;
-    // this.investments.forEach(val => {this.totalInvestment += val.currentValue; });
   }
 
-  getNetWorth(): void {
-    this.netWorth = this.totalCash + this.totalInvestment;
-  }
-
-  getTotalCash(): void {
-    this.totalCash = 3758.77;
-  }
-
-  getTotalInvestment(): void {
-    this.totalInvestment = 467826600;
-  }
 
   showDetails(investment): void {
     alert('Datetime: ' + investment.datetime + '\n' +
