4eead77706fa89cb92ec51b3d0bdd2a358971c24
d_quaner <849358686>
2020-09-17 11:37:43 +0800

    add - show investment details

--- src/app/investments/investments.component.ts
+++ src/app/investments/investments.component.ts
@@ -37,11 +37,15 @@ export class InvestmentsComponent implements OnInit {
         console.log(response);
         console.error('请求出错');
       });
-    // this.cashAccount.forEach(val => {this.totalCash += val.value; });
+    // this.totalCash = 0;
+    // for (const cash of this.cashAccount) {
+    //   alert(1);
+    // }
+    // this.cashAccount.forEach((val, index) => {this.totalCash += val.value; alert(index); });
   }
 
   getInvestment(): void {
-    const url = 'http://localhost:8080/investmentinfo';
+    const url = 'http://localhost:8080/investmentinfo/test';
     // 使用get方法请求url，请求一旦成功后，将调用传入的第一个方法；如果请求失败，将调用传入的第二个方法
     this.httpClient.get(url)
       .subscribe((response: any) => {
@@ -51,7 +55,8 @@ export class InvestmentsComponent implements OnInit {
         console.log(response);
         console.error('请求出错');
       });
-    // this.investments.forEach(val => {this.totalInvestment += val.value; });
+    // this.totalInvestment = 0;
+    // this.investments.forEach(val => {this.totalInvestment += val.currentValue; });
   }
 
   getNetWorth(): void {
@@ -59,11 +64,26 @@ export class InvestmentsComponent implements OnInit {
   }
 
   getTotalCash(): void {
-    this.totalCash = 5437;
+    this.totalCash = 3758.77;
   }
 
   getTotalInvestment(): void {
     this.totalInvestment = 467826600;
   }
 
+  showDetails(investment): void {
+    alert('Datetime: ' + investment.datetime + '\n' +
+      'Symbol: ' + investment.symbol + '\n' +
+      'Name: ' + investment.name + '\n' +
+      'Pre Close: ' + investment.pre_close + '\n' +
+      'Open: ' + investment.open + '\n' +
+      'Volume: ' + investment.volume + '\n' +
+      'Change: ' + investment.change + '\n' +
+      'Percent Change: ' + investment.percent_change + '\n' +
+      'High: ' + investment.high + '\n' +
+      'Low: ' + investment.low + '\n' +
+      'Purchase Price: ' + investment.purchasePrice.toFixed(2) + '\n' +
+      'Shares: ' + investment.shares + '\n' +
+      'Current Value: ' + investment.currentValue + '\n');
+  }
 }
